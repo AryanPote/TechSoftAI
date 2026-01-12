448758e5863c69f1800464b9ccf3436946fc94f7
AryanPote <luckypote14@gmail.com>
2026-01-12 13:18:02 +0530

    final

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-window.addEventListener("scroll", () => {
-  const navbar = document.querySelector(".navbar");
+const navbar = document.querySelector(".navbar");
 
-  if (window.scrollY > 50) {
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 30) {
     navbar.classList.add("scrolled");
   } else {
     navbar.classList.remove("scrolled");
